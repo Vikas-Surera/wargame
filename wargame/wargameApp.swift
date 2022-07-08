b3248095c07edd0cb18441e2ffe0844bb33a4400
Vikas Surera <vikassurera@gmail.com>
2022-07-08 23:07:44 +0530

    Initial Commit

---
+++ wargame/wargameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wargameApp.swift
+//  wargame
+//
+//  Created by Vikas Surera on 08/07/22.
+//
+
+import SwiftUI
+
+@main
+struct wargameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
